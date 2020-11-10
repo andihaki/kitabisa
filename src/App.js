69e83a1c7645cf160ea30687db57cbf307b06509
Andi Hakim Arif <andi.hakim.arif@gmail.com>
2020-11-10 19:56:13 +0700

    add emotion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { container } from './style';
 
 function App() {
-  return <div>Hello World</div>;
+  return <div className={container}>Hello World</div>;
 }
 
 export default App;
