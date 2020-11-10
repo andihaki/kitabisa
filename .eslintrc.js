f8aedc978427fbf3ceeca6dee641346101eb01d6
andi hakim arif <andi.hakim.arif@gmail.com>
2020-11-10 19:56:55 +0700

    add emotion (#1)

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
     'react',
   ],
   rules: {
+    'import/prefer-default-export': 'off',
   },
 };
---
+++ src/style.js
@@ -0,0 +1,8 @@
+import { css } from 'react-emotion';
+import { maxWidth } from './styles/const';
+
+export const container = css`
+  max-width: ${maxWidth};
+  margin: 0 auto;
+  width: 100%;
+`;
